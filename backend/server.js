a7afca54288d9e50a623e740b9d7be8b7aa685af
david-irvinne <davidneilleen@gmail.com>
2025-10-20 21:02:43 +0700

    feat: create backend backbone

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("test");
+import express from "express"
+import dotenv from "dotenv";
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/api/health", async(req, res) =>{
+  res.status(200).json({"success": true, "message": "david ganteng"});
+})
+
+app.listen(PORT, () => {
+  console.log("server is running on port", PORT);
+})
