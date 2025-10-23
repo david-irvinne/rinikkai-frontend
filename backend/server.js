bd1d635d88185ce54ed1d8fe1359ce96b34365da
david-irvinne <davidneilleen@gmail.com>
2025-10-24 01:12:08 +0700

    feat: let backend use static dist frontend in production

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,18 @@ import {connectDB} from "./lib/db.js";
 import articleRouter from "./routes/articles.js";
 import Article from "./models/article.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigin = process.env.ALLOWED_ORIGIN || "http://localhost:5173";
+
+// resolve __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({
     origin: allowedOrigin,
@@ -23,12 +30,19 @@ app.use(methodOverride('_method'));
 
 app.use("/articles", articleRouter);
 
+// Serve frontend build (React)
+const frontendPath = path.join(__dirname, "../frontend/dist");
+app.use(express.static(frontendPath));
+
 app.get('/', async (req, res) => {  
   const articles = await Article.find().sort({createdAt: 'desc'}); 
   res.render('articles/index', {articles: articles});
 })
 
-
+// Fallback: semua route non-API diarahkan ke React index.html
+app.use((req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log("server is running on port", PORT);
